6e7dccc1c6da04f25f562e52c678c7cb39376bca
Yibin Wei <yibinw.92@gmail.com>
2018-11-12 17:04:47 -0500

    first commit

---
+++ bamazon.js
@@ -0,0 +1,146 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "",
+    database: "bamazon_db"
+});
+  
+connection.connect(function(error){
+    if(error) throw error;
+
+    runBamazon();
+});
+
+function runBamazon(){
+    inquirer.prompt({
+        type: "list",
+        name: "menu",
+        message: "What would you like to do?",
+        choices: ["LIST","ORDER","SEARCH"]
+    }).then(function(response){
+        if(response.menu=="LIST"){listBamazon();}
+        if(response.menu=="ORDER"){orderBamazon();}
+        if(response.menu=="SEARCH"){searchBamazon();}
+    });
+}
+
+function listBamazon(){
+    console.log("Listing all items...");
+    console.log("---------------\n");
+    connection.query("SELECT * FROM products",function(error,res){
+        if(error) throw error;
+
+        for(var i=0; i<res.length; i++){
+            console.log(
+                "\nID: "+res[i].id+
+                "\n Item: "+res[i].item+
+                "\n Department: "+res[i].department+
+                "\n Price: $"+res[i].price+
+                "\n Quantity Left: "+res[i].stock
+            );
+            console.log("\n---------------");
+        }
+    }).then(inquirer.prompt({
+        type: "list",
+        name: "return",
+        message: "Do you wish to return to the menu?",
+        choices: ["Yes","No"]
+    }).then(function(response){
+        if(response.return == "Yes"){
+            console.log("Returning to the main menu...");
+            runBamazon();
+        } else {
+            console.log("Closing Bamazon...");
+            connection.end();
+        }
+    })
+    );
+}
+
+function searchBamazon(){
+    inquirer.prompt({
+        type: "input",
+        name: "search",
+        message: "Search by product ID: "
+    }).then(function(response){
+        connection.query("SELECT * FROM products WHERE id="+response.search,
+        function(error,res){
+            if(error) throw error;
+
+            console.log("\n ID: "+res.id+
+            "\n Item : "+res.item+
+            "\n Department: "+res.department+
+            "\n Price: $"+res.price);
+        })
+    }).then(inquirer.prompt({
+        type: "list",
+        name: "return",
+        message: "Do you wish to return to the menu?",
+        choices: ["Yes","No"]
+    }).then(function(response){
+        if(response.return == "Yes"){
+            console.log("Returning to the main menu...");
+            runBamazon();
+        } else {
+            console.log("Closing Bamazon...");
+            connection.end();
+        }
+    })
+    );
+}
+
+function orderBamazon(){
+    var quantity;
+    var location;
+    inquirer.prompt({
+        type: "list",
+        name: "order",
+        message: "Which item do you want to order? Search via ID"
+    }).then(function(response){
+        connection.query("SELECT * FROM products WHERE id="+response.search,
+        function(error,res){
+            if (error) throw error;
+            quantity = res.stock;
+            location = res.id;
+            if(quantity>0){
+                console.log("Ordering...");
+                quantity--;
+                connection.query("UPDATE products SET ? WHERE ?",
+                [{
+                    stock: quantity
+                },{
+                    is: location
+                }], function(error){
+                    if (error) throw error;
+                    console.log("Product ordered!");
+                });
+            } else {
+                console.log("There's not enough left, sorry!");
+            }
+        })
+    }).then(inquirer.prompt({
+        type: "list",
+        name: "return",
+        message: "Do you wish to return to the menu?",
+        choices: ["Yes","No"]
+    }).then(function(response){
+        if(response.return == "Yes"){
+            console.log("Returning to the main menu...");
+            runBamazon();
+        } else {
+            console.log("Closing Bamazon...");
+            connection.end();
+        }
+    })   
+    );
+}
---
+++ bamazonDB.sql
@@ -0,0 +1,45 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+USE bamazon_db;
+
+CREATE TABLE products (
+	id INT AUTO_INCREMENT,
+    item VARCHAR (200) NOT NULL,
+    department VARCHAR (200) NOT NULL,
+    price DOUBLE (10) NOT NULL,
+    stock INT (10) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO products (item,department,price,stock)
+VALUES ("Sony MDR V6","Audio",80,2500);
+
+INSERT INTO products (item,department,price,stock)
+VALUES ("Sennheiser HD8 DJ","Audio",250,350);
+
+INSERT INTO products (item,department,price,stock)
+VALUES ("Logitech MX Master","Electronics",100,400);
+
+INSERT INTO products (item,department,price,stock)
+VALUES ("Corsair Scimitar RGB Pro","Electronics",80,150);
+
+INSERT INTO products (item,department,price,stock)
+VALUES ("Shifting Realms Board Game","Games",65,100);
+
+INSERT INTO products (item,department,price,stock)
+VALUES ("Magic: the Gathering, Dominaria Booster Box","Games",100,1200);
+
+INSERT INTO products (item,department,price,stock)
+VALUES ("DC Comics Deck-Building Game","Games",20,600);
+
+INSERT INTO products (item,department,price,stock)
+VALUES ("Urth Post-Shave Elixir","Men's Accessories",350,1000);
+
+INSERT INTO products (item,department,price,stock)
+VALUES ("Fossil Jumper/S Polarized Sunglasses","Men's Accessories",160,250);
+
+INSERT INTO products (item,department,price,stock)
+VALUES ("AKRacing MAX","Office Supplies",500,25);
+
+INSERT INTO products (item,department,price,stock)
+VALUES ("Visconti Van Gogh Collection Fountain Pen","Office Supplies",290,15);
